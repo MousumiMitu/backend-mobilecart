3803a780e6b19eab53ddad0eb72108f4621ddf7a
MousumiMitu <mousumitu@gmail.com>
2022-06-14 21:34:30 +0600

    backend

---
+++ product.js
@@ -0,0 +1,31 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 12 Pro",
+    brand: "Apple",
+    desc: "6.1-inch display",
+    price: 999,
+    image:
+      "https://res.cloudinary.com/dvleqlnef/image/upload/v1655127820/onlineshop/iphone12pro_uysrhm.jpg",
+  },
+  {
+    id: 2,
+    name: "iPhone 12",
+    brand: "Apple",
+    desc: "5.4-inch mini display",
+    price: 699,
+    image:
+      "https://res.cloudinary.com/dvleqlnef/image/upload/v1655127820/onlineshop/iphone12_y8i1cz.jpg",
+  },
+  {
+    id: 3,
+    name: "Galaxy S",
+    brand: "Samsung",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+      "https://res.cloudinary.com/dvleqlnef/image/upload/v1655127820/onlineshop/galaxyS_b4mwt3.png",
+  },
+];
+
+module.exports = products;
